6f92000b0328fe42331774cc5f4f7d718684c2e6
SC12138 <chen.song.1637@student.uu.se>
2021-08-05 14:22:34 +0000

    Lab1 first commit

---
+++ user/pingpong.c
@@ -0,0 +1,37 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char* argv[]){
+    int p1[2];  // p1[0]: read fd, p1[1]: write fd
+    int p2[2];
+    char buf;
+    int n;
+
+    pipe(p1);
+    pipe(p2);
+
+    if(fork()==0){ // parent
+        write(p1[1], &buf, 1);
+        n = read(p2[0], &buf, 1);
+        if(n==1){
+            printf("%d: received pong\n", getpid());
+            exit(0);
+        }
+        else{
+            exit(-1);
+        }
+    }    
+    else{ // child
+        n = read(p1[0], &buf, 1);
+        if(n==1){
+            printf("%d: received ping\n", getpid());
+            write(p2[1], &buf, 1);
+            exit(0);
+
+        }
+        else{
+            exit(-1);
+        }
+    }
+
+}
